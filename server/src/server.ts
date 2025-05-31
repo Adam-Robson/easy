63e2ec3b52a55ed36e12ff967f047d93b3086f58
Adam Robson <adamrayrobson@gmail.com>
2025-05-30 22:34:40 -0700

    feat: update package.json and server.ts to include CORS and Helmet middleware

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,16 +1,30 @@
 import express from 'express';
+import cors from 'cors';
+import helmet from 'helmet';
 import { config } from './config/config';
 import contactRoutes from './routes/contact';
 
 const app = express();
 
+// cors
+app.use(cors({
+  origin: 'http://localhost:3001',
+  credentials: true
+}));
+
+// headers
+app.use(helmet());
+
+// jason
 app.use(express.json());
+
+// routes
 app.use('/api/contact', contactRoutes);
 
 app.get('/', (_req, res) => {
-  res.send('Welcome to the Express + TypeScript server!');
+  res.send('This is an express & typescript server + tsx.');
 });
 
 app.listen(config.port, () => {
-  console.log(`Server running at ${config.dbHost}:${config.port}`);
+  console.log(`Server running at http://localhost:${config.port}`);
 });
